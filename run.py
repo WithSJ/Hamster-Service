b615c88c2905ff16ddf3d627807c28676410a887
Sandeep Jadam <withsj.in@gmail.com>
2021-01-19 14:52:05 +0530

    recive_request thread added

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from hamster_service.connection.reciver import Reciver
 # create reciver thread 
 reciver_thread = Thread(target=Reciver.start)
 
+recive_request = Thread(target=Reciver.recive_request)
 
 
 
