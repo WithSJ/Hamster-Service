04d1699c6bfe5e7f841cb1b097fa6a40076291cd
Sandeep Jadam <withsj.in@gmail.com>
2021-01-20 21:29:27 +0530

    Thread added in Sender and error handling in connect method

--- hamster_service/connection/sender.py
+++ hamster_service/connection/sender.py
@@ -2,17 +2,21 @@
 Hamster Sender Service
 """
 import socket
+import threading
 
 from hamster_service.connection.connector import Connector
 from hamster_service.connection.utils import PORT_NUMBER
 
 
-class Sender(Connector):
+class Sender(Connector,threading.Thread):
     def __init__(self):
         self.SOCKET = self.create_tcp_socket()        
     
     def connect(self,ip_address=""):
-        self.SOCKET.connect((ip_address,PORT_NUMBER))
+        try:
+            self.SOCKET.connect((ip_address,PORT_NUMBER))
+        except socket.error as msg:
+            print(f"Sender socket error {msg}")
     
     def send_data(self):
         """Send Data to connected Socket"""
