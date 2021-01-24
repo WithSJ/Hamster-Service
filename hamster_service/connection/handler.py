7709075261ef0360345fcaa6dbcdea73c7f8c7f6
Sandeep Jadam <withsj.in@gmail.com>
2021-01-24 17:10:13 +0530

    is_connected classs method added currently its void

--- hamster_service/connection/handler.py
+++ hamster_service/connection/handler.py
@@ -5,7 +5,13 @@ CONNECTIONS_LIST = list()
 
 class Connections_Handler(threading.Thread):
     def __init__(self):
+        """Handle or manage all connections in CONNECTIONS_LIST"""
         pass
     
     def run(self):
+        pass
+
+    @classmethod
+    def is_connected(cls,socket):
+        """Check connection is live or not."""
         pass
