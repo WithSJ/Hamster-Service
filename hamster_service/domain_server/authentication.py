c7351aa9fb679093d59414efc8ec291a0b6bfc51
Sandeep Jadam <withsj.in@gmail.com>
2021-02-01 01:18:59 +0530

    Login Auth added

--- hamster_service/domain_server/authentication.py
+++ hamster_service/domain_server/authentication.py
@@ -13,7 +13,30 @@ def SignUp(email,password):
     try:
         user=Auth.create_user_with_email_and_password(email,password)
         Auth.send_email_verification(user["idToken"])
-        return {"message":"Check your email for verification."}
+        return {"message":"Account created. Check your email for verification."}
     except:
         return {"message":"EMAIL EXISTS"}
+    
+def Login(email,password):
+    if not Validator.isValidEmail(email):
+        return {"message":"Not a valid email."}
+
+    if not Validator.isValidPassword(password):
+        return {"message":"Not a valid password."}
+
+    try:
+        user = Auth.sign_in_with_email_and_password(email,password)
+        userInfo = Auth.get_account_info(user["idToken"])
+        
+        if userInfo["users"][0]["emailVerified"] != True:
+            Auth.send_email_verification(user["idToken"])
+            return {"message":"Email not verified. Check your email."}
+
+        return {
+            "message":"Login Successfull",
+            "user":user,
+            "userInfo": userInfo
+        }
+    except:
+        return {"message":"INVALID EMAIL or PASSWORD "}
     
