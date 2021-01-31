c8ef918aec7eb74b8a51b91d1808d0c6e2f3259f
Sandeep Jadam <withsj.in@gmail.com>
2021-02-01 01:35:46 +0530

    PasswordRest added in authentication

--- hamster_service/domain_server/authentication.py
+++ hamster_service/domain_server/authentication.py
@@ -27,7 +27,7 @@ def Login(email,password):
     try:
         user = Auth.sign_in_with_email_and_password(email,password)
         userInfo = Auth.get_account_info(user["idToken"])
-        
+
         if userInfo["users"][0]["emailVerified"] != True:
             Auth.send_email_verification(user["idToken"])
             return {"message":"Email not verified. Check your email."}
@@ -40,3 +40,12 @@ def Login(email,password):
     except:
         return {"message":"INVALID EMAIL or PASSWORD "}
     
+def PasswordReset(email):
+    if not Validator.isValidEmail(email):
+        return {"message":"Not a valid email."}
+
+    try:
+        Auth.send_password_reset_email(email)
+        return {"message":"Check email for reset password."}
+    except:
+        return {"message":"EMAIL NOT FOUND"}
