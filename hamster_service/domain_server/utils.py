be74233630187fd0f5fc998fbabaf57adf89339b
Sandeep Jadam <withsj.in@gmail.com>
2021-02-01 12:09:03 +0530

    User class contain current logedin user data

--- hamster_service/domain_server/utils.py
+++ hamster_service/domain_server/utils.py
@@ -1,4 +1,25 @@
 import re
+import json
+
+class User:
+    def __init__(self,user,userInfo):
+        self.localId = user["localId"]
+        self.email = user["email"]
+        self.displayName = user["displayName"]
+        self.idToken = user["idToken"]
+        self.registered = user["registered"]
+        self.refreshToken = user["refreshToken"]
+        self.expiresIn = user["expiresIn"]
+        
+        self.passwordHash = userInfo["users"][0]["passwordHash"]
+        self.emailVerified = userInfo["users"][0]["emailVerified"]
+        self.passwordUpdatedAt = userInfo["users"][0]["passwordUpdatedAt"]
+        self.providerUserInfo = userInfo["users"][0]["providerUserInfo"]
+        self.validSince = userInfo["users"][0]["validSince"]
+        self.lastLoginAt = userInfo["users"][0]["lastLoginAt"]
+        self.createdAt = userInfo["users"][0]["createdAt"]
+        self.lastRefreshAt = userInfo["users"][0]["lastRefreshAt"]
+
 
 class Validator:
     @classmethod
